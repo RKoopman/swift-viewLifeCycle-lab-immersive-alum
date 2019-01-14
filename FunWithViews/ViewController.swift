7c83ede1d54428ce942e517e45ab576939d49a86
Raoul Koopman <raoul@fubo.tv>
2019-01-14 15:31:16 -0500

    fixed topDice and actually show results

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -29,39 +29,118 @@ class ViewController: UIViewController {
     @IBOutlet weak var rollFive: UILabel!
     @IBOutlet weak var rollSix: UILabel!
     
+    var totalRolls = 0
+    var currentRoll = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideDie()
+        hideRollsResults()
+        print("total rolls : \(totalRolls)")
     }
+    
     
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
-        arrangeDie(roll: randomDiceRoll())
+        totalRolls += 1
+        currentRoll = randomDiceRoll()
+        hideDie()
+        rearrangeDie(roll: currentRoll)
+        fillResults(roll: currentRoll)
+        print("currentRoll = \(currentRoll)")
     }
     
+    // Returns back a random Int (1, 2, 3, 4, 5, or 6)
+    func randomDiceRoll() -> Int {
+        return Int(arc4random_uniform(6) + 1)
+    }
     
-    func arrangeDie (roll: Int) {
-        
+    // fill out results
+    func fillResults(roll: Int) {
+        switch totalRolls {
+        case 1:
+            rollOne.text = "\(currentRoll)"
+            rollOne.isHidden = false
+            print("roll label hit :: \(roll)")
+        case 2:
+            rollTwo.text = "\(currentRoll)"
+            rollTwo.isHidden = false
+            print("roll label hit :: \(roll)")
+        case 3:
+            rollThree.text = "\(currentRoll)"
+            rollThree.isHidden = false
+            print("roll label hit :: \(roll)")
+        case 4:
+            rollFour.text = "\(currentRoll)"
+            rollFour.isHidden = false
+            print("roll label hit :: \(roll)")
+        case 5:
+            rollFive.text = "\(currentRoll)"
+            rollFive.isHidden = false
+            print("roll label hit :: \(roll)")
+        case 6:
+            rollSix.text = "\(currentRoll)"
+            rollSix.isHidden = false
+            print("roll label hit :: \(roll)")
+        default:
+            hideRollsResults()
+        }
+    }
+    
+    // hides top die & results labels upon startup
+    func hideDie() {
+        viewOne.isHidden = true
+        viewTwo.isHidden = true
+        viewThree.isHidden = true
+        viewFour.isHidden = true
+        viewFive.isHidden = true
+        viewSix.isHidden = true
+        ViewSev.isHidden = true
+        viewEight.isHidden = true
+        viewNine.isHidden = true
+    }
+    func hideRollsResults() {
+        rollOne.isHidden = true
+        rollTwo.isHidden = true
+        rollThree.isHidden = true
+        rollFour.isHidden = true
+        rollFive.isHidden = true
+        rollSix.isHidden = true
+
+    }
+    
+    // handles the top die
+    func rearrangeDie (roll: Int) {
         switch roll {
         case 1:
             viewFive.isHidden = false
+            print("rolled \(roll)!")
+            print("total rolls : \(totalRolls) \n---------------")
         case 2:
             viewOne.isHidden = false
             viewNine.isHidden = false
+            print("rolled \(roll)!")
+            print("total rolls : \(totalRolls) \n---------------")
         case 3:
             viewOne.isHidden = false
             viewFive.isHidden = false
             viewNine.isHidden = false
+            print("rolled \(roll)!")
+            print("total rolls : \(totalRolls) \n---------------")
         case 4:
             viewOne.isHidden = false
             viewThree.isHidden = false
             ViewSev.isHidden = false
             viewNine.isHidden = false
+            print("rolled \(roll)!")
+            print("total rolls : \(totalRolls) \n---------------")
         case 5:
             viewOne.isHidden = false
             viewThree.isHidden = false
             viewFive.isHidden = false
             ViewSev.isHidden = false
             viewNine.isHidden = false
+            print("rolled \(roll)!")
+            print("total rolls : \(totalRolls) \n---------------")
         case 6:
             viewOne.isHidden = false
             viewThree.isHidden = false
@@ -69,6 +148,8 @@ class ViewController: UIViewController {
             viewSix.isHidden = false
             ViewSev.isHidden = false
             viewNine.isHidden = false
+            print("rolled \(roll)!")
+            print("total rolls : \(totalRolls) \n---------------")
         default:
             viewOne.isHidden = true
             viewTwo.isHidden = true
@@ -79,13 +160,7 @@ class ViewController: UIViewController {
             ViewSev.isHidden = true
             viewEight.isHidden = true
             viewNine.isHidden = true
+            print("total rolls : \(totalRolls) \n---------------")
         }
     }
-    
-    
-    // Returns back a random Int (1, 2, 3, 4, 5, or 6)
-    func randomDiceRoll() -> Int {
-        return Int(arc4random_uniform(6) + 1)
-    }
-
 }
